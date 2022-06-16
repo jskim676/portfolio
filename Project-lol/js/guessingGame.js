25951480f2cfce76489b55d0b8e50c846be247d7
jskim676 <wkxndlsvk@gmail.com>
2022-06-16 18:00:20 +0900

    edit

--- Project-lol/js/guessingGame.js
+++ Project-lol/js/guessingGame.js
@@ -4,12 +4,19 @@ ajax.send();
 const squareKey = JSON.parse(ajax.response).slice(1);
 const squareIcon = document.getElementById('squareIcon');
 
-let squareNumber = 0;
+let squareNumber = Math.floor(Math.random()*squareKey.length);
+let getData = true;
+console.log(squareKey);
+console.log(squareNumber);
 const squareBtn = document.getElementById('squareBtn');
 squareBtn.addEventListener('click',function() {
   squareIcon.classList.remove('hidden');
-  squareNumber = Math.floor(Math.random()*squareKey.length);
+  if(getData === false) {
+    squareNumber = Math.floor(Math.random()*squareKey.length);
+  }
   const squareData = squareKey[squareNumber].squarePortraitPath;
   const squareIllust = squareData.slice(squareData.indexOf("assets/")+7,squareData.length);
   squareIcon.src = "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/" + squareIllust;
+  console.log(squareIllust);
+  getData = false;
 });
