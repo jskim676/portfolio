5557e136a9dd75548ae90ff5cc1b55076c936fca
Kim Jun Sik <wkxndlsvk@gmail.com>
2022-07-29 03:04:19 +0900

    gongyoo slider almost done

--- GongYoo/js/advertisement.js
+++ GongYoo/js/advertisement.js
@@ -1,13 +1,20 @@
 function ad () {
   const slider = Array.from(document.getElementsByClassName('slider'));
 
-  console.log(slider[0].children);
-
   let translateValue = 0;
   const timer = setInterval(()=> {
-    // translateValue++;
-    // slider[0].style.transform = `translate(-${translateValue*100}%)`;
-  },3000);
+    translateValue++;
+    slider[0].style.transform = `translate(-${translateValue*100}%)`;
+    slider[1].style.transform = `translate(-${translateValue*25}%)`;
+    if(translateValue >= slider[0].children.length-1) {
+      translateValue = -1;
+    }
+
+    // slider[1].style.transform = `translate(-${translateValue*25}%)`;
+    // if(translateValue >= slider[1].children.length-1) {
+    //   translateValue = -1;
+    // }
+  },5000);
 }
 
 export default ad
